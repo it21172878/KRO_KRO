c7a179eb0d549c30a6b53e4da4189b18b1b75df6
Dilanka <it21172878@my.sliit.lk>
2023-04-30 19:28:09 +0530

    KRO-Cloths-App-React

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">KRO Cloths</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
